6cc5b3b121c3de9f7a15c588b19a967918694a52
Spencer_Vail <vail.spencer@gmail.com>
2020-07-07 13:37:49 -0700

    Animal Abstract created

---
+++ Animals/src/animalsjava/Animal.java
@@ -0,0 +1,34 @@
+package animalsjava;
+
+abstract class Animal{
+
+	protected static int maxId;
+	protected int food;
+	protected int id;
+	protected String name;
+	protected int yearDiscovered;
+
+	abstract String canBreath();
+	abstract String canMove();
+	abstract String makeBabies();
+
+	void eatFood(int i){
+		this.food -= food;
+	}
+
+	String getName(){
+		this.name = name;
+	}
+
+	int getYear(){
+		return yearDiscovered;
+	}
+
+	public Animal(String name, int yearDiscovered){
+		id = maxId++;
+		this.name = name;
+		this.yearDiscovered = yearDiscovered;
+	}
+	
+	
+}
